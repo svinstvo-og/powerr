4b900b6af3e14f8cf9d0c19c5af006f44ae1b9b0
svinstvo-og <maxim12.05.2006koval@gmail.com>
2024-10-25 00:50:41 +0200

    Inmplemented some db entities

---
+++ src/main/java/pj/powerr/entity/Excercise.java
@@ -0,0 +1,43 @@
+package pj.powerr.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Excercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String muscleGroup;
+
+    public Long getId(Excercise excercise) {
+        return excercise.id;
+    }
+
+    public String getName(Excercise excercise) {
+        return excercise.name;
+    }
+
+    public String getMuscleGroup(Excercise excercise) {
+        return excercise.muscleGroup;
+    }
+
+    public Void setName(Excercise excercise, String name) {
+        this.name = name;
+    }
+
+    public Void setMuscleGroup(Excercise excercise, String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public Excercise () {}
+
+    public Excercise(String name, String muscleGroup) {
+        this.name = name;
+        this.muscleGroup = muscleGroup;
+    }
+}
